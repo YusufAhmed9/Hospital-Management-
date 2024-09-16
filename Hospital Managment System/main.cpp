ce0b5b041c3868f3ce9cc25e2b026e6428c0211a
Yusuf Ahmed <163534012+YusufAhmed9@users.noreply.github.com>
2024-09-17 00:52:11 +0300

    Initial template

--- Hospital Managment System/main.cpp
+++ Hospital Managment System/main.cpp
@@ -1,19 +1,73 @@
 #include<iostream>
 #include<string>
 #include<vector>
+
 using namespace std;
-class Reservation {
+
+class Date {
+
 
 };
+
+class Reservation {
+private:
+	string reservationID;
+	string hospitalID;
+	string reservationType;
+	Date reservationDay;
+
+};
+
 class User {
 private:
 	string userID;
 	string username;
 	string password;
+public:
+	User() : 
+		userID("User ID"), username("Username"), password("Password") {}
+
+	User(string userID, string username, string password) :
+		userID(userID), username(username), password(password){}
+
+	void setUsername(string username) {
+		this->username = username;
+	}
+
+	void setUserID(string userID) {
+		this->userID = userID;
+	}
+
+	void setPassword(string password) {
+		this->password = password;
+	}
+
+	string getUsername() {
+		return username;
+	}
+
+	string getUserID() const {
+		return userID;
+	}
+
+	string getPassword() const {
+		return password;
+	}
 };
+
 class Patient : public User {
 private:
-	vector< Reservation>reservations;
+	vector<Reservation>reservations;
+public:
+	void reserve(Reservation reservation) {
+		reservations.push_back(reservation);
+	}
 };
+
+class Admin : public User {
+private:
+
+};
+
 int main() {
 }
