e531a0385ad6eb1b812ba81747133d2646f98136
Yusuf Ahmed <163534012+YusufAhmed9@users.noreply.github.com>
2024-09-21 11:20:33 +0300

    Finishing the sign up and uploading it to a file

--- Hospital Managment System/main.cpp
+++ Hospital Managment System/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<fstream>
+#include<sstream>
 
 using namespace std;
 
@@ -20,33 +22,24 @@ private:
 
 class User {
 private:
-	int userID;
 	string username;
 	string password;
 
 public:
-	static int IDGenerator;
-	User() {
-
+	User():
+		username("Username"), password("Password"){
 	}
 
 	User(string username, string password) :
 		username(username), password(password) {
-		userID = IDGenerator++;
 	}
 
-	bool setUsername(const string& username) {
-		if (username.empty())
-			return 0;
+	void setUsername(const string& username) {
 		this->username = username;
-		return 1;
 	}
 
-	bool setPassword(const string& password) {
-		if (password.size() < 8)
-			return 0;
+	void setPassword(const string& password) {
 		this->password = password;
-		return 1;
 	}
 
 	string getUsername() const {
@@ -56,8 +49,11 @@ public:
 	string getPassword() const {
 		return password;
 	}
+
+	virtual bool userType() {
+		return false;
+	}
 };
-int User::IDGenerator = 0;
 
 class Patient : public User {
 private:
@@ -68,14 +64,32 @@ public:
 	void reserve(Reservation reservation) {
 		reservations.push_back(reservation);
 	}
+
+	bool userType() override {
+		return true;
+	}
 };
 
 class Admin : public User {
 private:
+public:
+	Admin(User user) : User(user) {}
 
+	bool userType() override {
+		return false;
+	}
 };
 
 class DataBase {
+private:
+	void loadToFile(const string& fileName, User* user){
+		ofstream file(fileName);
+		file << user->getUsername() << ',';
+		file << user->getPassword() << ',';
+		file << user->userType() << ',';
+		file << '\n';
+		file.close();
+	}
 public:
 	void mainInterface() {
 		int type;
@@ -85,7 +99,7 @@ public:
 			
 			cin >> type;
 			
-			while (type <= 0 or type > 2) {
+			while (type != 1 and type != 2) {
 				cout << "Error: please enter a valid option\n";
 				cout << "Welcome to HMS\n"
 					<< "1. Login\n"
@@ -99,7 +113,13 @@ public:
 				signUp();
 	}
 	void login() {
-
+		cout << "***********************\n\n\Login\n\n***********************\n\n";
+		string username;
+		string password;
+		cout << "Username: ";
+		cin >> username;
+		cout << "Password: ";
+		cin >> password;
 	}
 
 	void signUp() {
@@ -114,49 +134,50 @@ public:
 			cout << "Username: ";
 			cin >> tempUsername;
 			
-			//If the enterd username is empty it returns 0
-			if (!user.setUsername(tempUsername)) {
+			if (tempUsername.empty()) {
 				cout << "Please enter another user name\n";
 				continue;
 			}
+
+			user.setUsername(tempUsername);
 			break;
 		}
 
 		while (true) {
 			cout << "Password: ";
 			cin >> tempPassword;
+
 			cout << "Confirm Password: ";
 			cin >> tempConfirmPassword;
 			
-			//Checking if the entered passwords are equal
 			if (tempPassword != tempConfirmPassword) {
 				cout << "Passwords don't match\n";
 				continue;
 			}
-			//If the password is weak the condition returns 0
-			if (!user.setPassword(tempPassword)) {
+
+			if (tempPassword.size() < 8) {
 				cout << "The password you've enterd is weak\n";
 				continue;
 			}
+			user.setPassword(tempPassword);
 			break;
 		}
 
-		while (true) {
-			cout << "User type (1. Patient/ 2. Admin): ";
-			cin >> userType;
-			
-			if (userType != 1 and userType != 2) {
-				cout << "Please enter a valid user type\n";
-				continue;
-			}
-			break;
-		}
+		User* user1;
+
+		ifstream file("Data.txt");
+
+		//The user is an admin "Because it is the first user"
+		if (file.peek() == std::ifstream::traits_type::eof())
+			user1 = new Admin(user);
+
 		//The user is a patient
-		if (userType == 1)
-			Patient p1(user);
-		
-		//The user is an admin
-		else;
+		else
+			user1 = new Patient(user);
+
+		loadToFile("Data.csv", user1);
+
+		delete user1;
 	}
 };
 
