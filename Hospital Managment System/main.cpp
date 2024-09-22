3146be408ebc9fc05be949ca3a4c88f58f643953
Yusuf Ahmed <163534012+YusufAhmed9@users.noreply.github.com>
2024-09-22 16:01:41 +0300

    Cleaner variable naming, Adding Login function logic, Searching for user in file

--- Hospital Managment System/main.cpp
+++ Hospital Managment System/main.cpp
@@ -7,8 +7,6 @@
 using namespace std;
 
 class Date {
-
-
 };
 
 class Reservation {
@@ -17,7 +15,6 @@ private:
 	string hospitalID;
 	string reservationType;
 	Date reservationDay;
-
 };
 
 class User {
@@ -50,9 +47,20 @@ public:
 		return password;
 	}
 
-	virtual bool userType() const{ // A function to tell if the
-							 // The user is a patient '1'
-							// or an admin '0'
+	virtual bool userType() const{ 
+		// A function to tell if the
+		// The user is a patient '1'
+		// or an admin '0'
+		return false;
+	}
+};
+
+class Admin : public User {
+private:
+public:
+	Admin(User user) : User(user) {}
+
+	bool userType() const override {
 		return false;
 	}
 };
@@ -72,45 +80,100 @@ public:
 	}
 };
 
-class Admin : public User {
+class Database {
 private:
-public:
-	Admin(User user) : User(user) {}
+	const int loginChoice = 1;
+	const int signUpChoice = 2;
+	const int usernameInSheetEnd = 2;
+	const int usernameInSheetStart = 1;
+	const int passwordInSheetStart = 2;
+	const int passwordInSheetEnd = 3;
+	const string sheetName = "Data.csv";
 
-	bool userType() const override {
-		return false;
-	}
-};
 
-class DataBase {
-private:
-	void loadToFile(const string& fileName, User* user) const{
-		//To count number of rows
-		//To make an ID for each user
-		ifstream in(fileName);
+	int countRowsInFile(const string& fileName) const {
 		int row = 0;
 		string temp;
+		ifstream in(fileName);
 		while (getline(in, temp))
 			++row;
-		temp.clear();
+		return row;
+	}
 
+	bool fileIsEmpty(const string& fileName) const {
+		return countRowsInFile(fileName) == 1;
+	}
 
+	void loadToFile(const string& fileName, User* user) const {
 		// Id format : User type(A admin, P patient) + Row number
-		ofstream file(fileName, ios::app);
+		string temp;
 		temp.push_back(user->userType() ? 'P' : 'A');
-		temp.push_back(row + '0');
+		temp.push_back(countRowsInFile(fileName) + '0');
 
+		ofstream file(fileName, ios::app);
 		file << temp << ',';
 		file << user->getUsername() << ',';
 		file << user->getPassword() << ',';
 		file << user->userType() << ',';
 		file << '\n';
+
 		file.close();
 	}
 
-	bool userExists(const string& fileName,const string& username) const {
+	bool userInfoChecking(const string& fileName,const string& username) const {
+		string temp;
 		ifstream file(fileName);
 
+		//Using a string to iterate over the created sheet
+		while (getline(file, temp)) {
+			//To count commas since it is comma seperated
+			int commacnt = 0;
+			string usernameCheck;
+			
+			for (char& s : temp) {
+				commacnt += s == ',';
+
+				if (commacnt == usernameInSheetStart and s != ',')
+					usernameCheck.push_back(s);
+
+				if (commacnt == usernameInSheetEnd)
+					break;
+			}
+
+			if (usernameCheck == username)//Username was found the created sheet
+				return true;
+		}
+
+		return false;
+	}
+	
+	bool userInfoChecking(const string& fileName,const string& username, const string& password) const {
+		string temp;
+		ifstream file(fileName);
+
+		//Using a string to iterate over the created sheet
+		while (getline(file, temp)) {
+			//To count commas since it is comma seperated
+			int commacnt = 0;
+			string usernameCheck;
+			string passwordCheck;
+			
+			for (char& s : temp) {
+				commacnt += s == ',';
+
+				if (commacnt == usernameInSheetStart and s != ',')
+					usernameCheck.push_back(s);
+
+				if (commacnt == passwordInSheetStart and s != ',')
+					passwordCheck.push_back(s);
+
+				if (commacnt == passwordInSheetEnd)
+					break;
+			}
+
+			if (usernameCheck == username and passwordCheck == password)//Username was found the created sheet
+				return true;
+		}
 
 		return false;
 	}
@@ -119,10 +182,17 @@ private:
 		cout << "***********************\n\n\Login\n\n***********************\n\n";
 		string username;
 		string password;
-		cout << "Username: ";
-		cin >> username;
-		cout << "Password: ";
-		cin >> password;
+		while (true) {
+			cout << "Username: ";
+			cin >> username;
+			cout << "Password: ";
+			cin >> password;
+			if (!userInfoChecking(sheetName, username, password)) {
+				cout << "Username and Password do not match\n";
+				continue;
+			}
+			break;
+		}
 	}
 
 	void signUp() {
@@ -136,7 +206,7 @@ private:
 			cout << "Username: ";
 			cin >> tempUsername;
 			
-			if (tempUsername.empty()) {
+			if (tempUsername.empty() or userInfoChecking(sheetName, tempUsername)) {
 				cout << "Please enter another user name\n";
 				continue;
 			}
@@ -167,34 +237,32 @@ private:
 
 		User* user1;
 
-		ifstream file("Data.txt");
-		int row = 0;
-		string temp;
-		while (getline(file, temp))
-			++row;
-
 		//The user is an admin "Because it is the first user"
-		if (row == 0)
+		if (fileIsEmpty(sheetName))
 			user1 = new Admin(user);
 
 		//The user is a patient
 		else
 			user1 = new Patient(user);
 
-		loadToFile("Data.csv", user1);
+		loadToFile(sheetName, user1);
 
 		delete user1;
 	}
+
 public:
 	void mainInterface() {
 		int type;
-			cout << "Welcome to HMS\n\n"
-				<< "1. Login\n"
-				<< "2. Sign Up\n";
-			cout << "Enter choice: ";
+		cout << "**********************************************************\n\n";
+		cout << "\tWelcome to Hospital Management System(HMS)\n\n";
+		cout << "**********************************************************\n\n";
+		cout << "1. Login\n"
+			<< "2. Sign Up\n\n";
+		
+		cout << "Enter choice: ";
 			cin >> type;
 			
-			while (type != 1 and type != 2) {
+			while (type != loginChoice and type != signUpChoice) {
 				cout << "Error: please enter a valid option\n";
 				cout << "Welcome to HMS\n"
 					<< "1. Login\n"
@@ -202,7 +270,7 @@ public:
 				cin >> type;
 			}
 
-			if (type == 1)
+			if (type == loginChoice)
 				login();
 			else
 				signUp();
@@ -210,6 +278,7 @@ public:
 };
 
 int main() {
-	DataBase DB;
+	Database DB;
 	DB.mainInterface();
+	return 0;
 }
